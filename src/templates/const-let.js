40626956fe90f90272d4a8ef0272725bc487e4c7
iOznny <f000170014@gmail.com>
2021-07-07 20:17:37 -0500

    Cambio

---
+++ src/templates/const-let.js
@@ -0,0 +1,16 @@
+// Variables y Constantes
+const name = 'Demo';
+const lastname = 'Demo';
+
+let dado = 5;
+dado = 4;
+
+console.log(name, lastname, dado);
+
+// Variables de Scout.
+if (true) {
+    let dado = 6;
+    console.log(dado);
+}
+
+console.log(dado);
