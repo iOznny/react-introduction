119305a05cce9f347a9e21c709abedde7de01593
iOznny <f000170014@gmail.com>
2021-07-08 19:28:14 -0500

    c

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 // Funciones
-function greet(name) {
+const greet = function(name) {
     return `Hola, ${ name }`;
 }
 
-console.log(greet('Demo'));
+// Funciones de flecha o lamba.
+const arrow = (name) => {
+    return `Hola, ${ name }`;
+}
+
+const arrowReturn = (name) => `Hola, ${ name }`;
+const helloWorld = () => `Hola Mundo`;
+
+console.log(greet);
+console.log(arrow('Arrow'));
+console.log(arrowReturn('arrowReturn'));
+console.log(helloWorld());
+
+// Examples
+const getUser = () => {
+    return {
+        uid: 'abc123',
+        name: 'Chupapimuñaño'
+    }
+}
+
+const getUserArror = () => ({
+    uid: 'abc123',
+    name: 'Chupapimuñaño'
+});
+
+
+console.log(getUser());
