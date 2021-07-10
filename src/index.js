d5ffecfb7f8c539d30f0d9f4e75cd709c45cf029
iOznny <f000170014@gmail.com>
2021-07-10 13:34:56 -0500

    Cambio

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 // Promises
 import { getHeroeById } from "./templates/import-export-functions";
 
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {    
-        const heroe = getHeroeById(2);        
-        resolve(heroe);
-    }, 2000);
-});
+// const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {    
+//         const heroe = getHeroeById(2);        
+//         resolve(heroe);
+//         //reject('No se pudo encontrar el heroe.');
+//     }, 2000);
+// });
 
-promise.then((heroe) => {
-    console.log('Heroe', heroe);
-});
+// promise.then((heroe) => {
+//     console.log('Heroe', heroe);
+// }).catch(e => console.warn(e));
+
+const getHeroeByIdAsync = (id) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {    
+            const heroe = getHeroeById(id);            
+            (heroe) ? resolve(heroe) : reject('No se pudo encontrar el heroe');
+        }, 2000);
+    });
+};
+
+getHeroeByIdAsync(1)
+    .then(console.log)
+    .catch(console.warn);
